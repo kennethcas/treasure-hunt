8cdf451eedbc51dc08b94a942969d03d3d7f6e61
noodlexmurder <kc4338@nyu.edu>
2022-02-16 14:42:37 -0500

    added sprites

---
+++ Treasure Hunt Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if right arrow, sprite reflected on y axis
+        //if left arrow, sprite stays the same 
+
+        //if right, go right
+        //if left, go left
+    }
+}
