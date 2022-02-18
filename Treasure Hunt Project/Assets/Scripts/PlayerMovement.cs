71475a33b5275a8a43b24c23d25084a655e68bbf
noodlexmurder <82953324+noodlexmurder@users.noreply.github.com>
2022-02-18 16:13:57 -0500

    added player controls

--- Treasure Hunt Project/Assets/Scripts/PlayerMovement.cs
+++ Treasure Hunt Project/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float jumpStrength = 5.0f;
+
+    [SerializeField]
+    float movementSpeed = 5.0f;
+    float moveX;
+
+    bool canJump = false;
+    bool isGrounded = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void PlayerControls()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            canJump = true;
+        }
+    }
+
+    void Jump()
+    {
+        if (!isGrounded)
+        {
+            return; //just stop and dont cgeck anything else underneath it
+        }
+
+        isGrounded = false;
+        rb.AddForce(Vector2.up * jumpStrength, ForeMode2D.Impulse); //shorthand for Vector2(0,1)
+        Debug.Log("Jump!", gameObject); //lights up thing that printed the message
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+        Debug.Log("Hit something", collision.gameObject);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveX * movementSpeed, rb.velocity.y);
+        if (canJump == true)
+        {
+            canJump = false;
+            Jump();
+        }
+    }
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        PlayerControls();
         //if right arrow, sprite reflected on y axis
         //if left arrow, sprite stays the same 
 
