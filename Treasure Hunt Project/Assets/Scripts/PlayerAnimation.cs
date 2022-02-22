376d86f46cf35449f464d2fca2c49eed23caae17
noodlexmurder <82953324+noodlexmurder@users.noreply.github.com>
2022-02-21 19:48:42 -0500

    Started dialogue system

--- Treasure Hunt Project/Assets/Scripts/PlayerAnimation.cs
+++ Treasure Hunt Project/Assets/Scripts/PlayerAnimation.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-   //ublic Animator animator;
-    void Start()
-    {
-        
-    }
+    public AudioSource bleepSource;
 
-   
-    void Update()
+    private void Start()
     {
+        bleepSource = GetComponent<AudioSource>();
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+       // if (other.CompareTag("Player"))
+        
+            bleepSource.Play();
+        
         
     }
 }
