e069fcd07dab26401a0a1f72da47675d41b68c96
noodlexmurder <82953324+noodlexmurder@users.noreply.github.com>
2022-02-21 22:39:17 -0500

    Final touches

---
+++ Treasure Hunt Project/Assets/Scripts/Trigger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Trigger : MonoBehaviour
+{
+    BoxCollider2D boxCollider;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+    void Start()
+    {
+        boxCollider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
